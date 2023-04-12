a2c6b84b9182518d0750f3f5ef0bb3c5500cb91c
rachid <rachid@example.com>
2023-04-12 16:03:31 +0000

    first commit

---
+++ home.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home page</title>
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+    <section class="main">
+
+        <div class="box-container">
+        <nav>
+            <a href="#" class="logo">
+                <img src="images/logo1.png" alt=""/>
+            </a>
+            
+            <ul class="menu">
+                <li><a href="#" class="active" >Home</a></li>
+                <li><a href="#" >Features</a></li>
+                <li><a href="#" >About</a></li>
+                <li><a href="#" >Contact</a></li>
+        
+            </ul>
+
+        </nav>
+        <div class="main-heading">
+            <h1>Create Perspectives With Virtual Reality</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut atque nemo beatae saepe blanditiis.</p>
+            <a class="main-btn" href="#">Contact</a>
+        </div>
+        </div>
+    </section>
+    <section class="features">
+        <div class="feature-container">
+            <div class="feature-box">
+                <div class="f-img">
+                    <img src="images/info-icon1.png"/>
+                </div>
+                <div class="f-text">
+                    <h4>Web Development</h4>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <a href="#" class="main-btn">Check</a>
+                </div>
+            </div>
+
+            <div class="feature-box">
+                <div class="f-img">
+                    <img src="images/info-icon2.png"/>
+                </div>
+                <div class="f-text">
+                    <h4>Software Development</h4>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <a href="#" class="main-btn">Check</a>
+                </div>
+            </div>
+
+            <div class="feature-box">
+                <div class="f-img">
+                    <img src="images/info-icon3.png"/>
+                </div>
+                <div class="f-text">
+                    <h4>App Development</h4>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <a href="#" class="main-btn">Check</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="about">
+        <div class="about-img">
+            <img src="images/about.png">
+        </div>
+        <div class="about-text">
+            <h2>Start Tracking Your Statistics</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius doloremque animi sed quasi temporibus porro adipisci aspernatur quo nostrum in, corporis ab aut repellendus labore.</p>
+            <button class="main-btn">Read More</button>
+        </div>
+    </section>
+    <section class="contact">
+        <div class="contact-heading">
+            <h1>Contact Us</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam nulla sint illum?</p>
+        </div>
+        <form action="" method="post">
+            <input type="text" name="user" placeholder="Enter your name"/>
+            <input type="email" name="email" placeholder="Enter your E-mail"/>
+            <textarea name="message" placeholder="type you message here ..."></textarea>
+            <button class="main-btn">Continue</button>
+        </form>
+    </section>
+    
+</body>
+</html>
